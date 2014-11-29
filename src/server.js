7ae3216a23544783b7a43495148a41c50ce7cd1b
Ronnie Mitra <mitrapandey@MitraPandeys-MacBook-Pro.local>
2014-11-29 00:58:09 +0000

    fixed bugs, mongoskin problems

--- src/server.js
+++ src/server.js
@@ -40,6 +40,7 @@ var alpsParser = require('./alps/alps.js');
 
 // Handlers
 require('./handlers/resources.js')(app, conn);
+require('./handlers/states.js')(app, conn);
 require('./handlers/map.js')(app, conn);
   
 app.get('/', function(req, res){
@@ -197,274 +198,6 @@ function authorizeUser(req, res, next) {
 
 
 // API
-
-// RESOURCES
-/**
-app.get('/projects/:projectId/resources', passport.authenticate('bearer', {session: false}), function(req, res) {
-    var projectId = req.params.projectId;
-   
-    // How do we chain promises?
-    isOwner(projectId, req.user)
-    .then(retrieve('resources', {'project' : projectId})).then(function() {
-    }, 
-   function(error) {
-   });
-        
-});
-**/
-
-// STATES
-app.get('/projects/:projectId/states', passport.authenticate('bearer', {session: false}), authorizeUser, function(req, res) {
-    
-	//TODO: turn this AZ step into an express processor
-  var projectId = req.params.projectId;
-    console.log(projectId);
-    
-    if( projectId == null ) {
-        res.send(400, '{"message": "invalid project ID"}');
-    }
-  
-   conn.collection('tasks').find({project: projectId}).toArray(function (err, tasks) {
-	   if( err ) {
-		   console.log(err);
-		   res.status(500);
-		   res.send("Unable to retrieve data.");
-	   }else {
-		   // Get transitions and include them in the object.
-		   // TODO: When I re-engineer this backend, I should use RSVP or a relational database or osmething better than this crap...
-		   console.log(tasks);
-		   console.log(tasks.length);
-		   
-		   if( tasks.length === 0 ){
-			   res.send(200, tasks);
-		   }
-		   
-		   var responseCounter = tasks.length;
-		   stateMap = [];
-		   for( var i = 0; i < tasks.length; i++) {
-			   stateMap[tasks[i]._id.toString()] = i;
-		   }
-		   
-		   
-		   function addTransition(error, transitions) {
-			   if( error != null ) {
-				   console.log(error);
-				   res.status(500);
-				   res.send('seomthing went wrong');
-			   } else {
-				   responseCounter--;
-				   if( transitions.length > 0 ) {
-					   var index = stateMap[transitions[0].source];
-					   tasks[index].transitions = transitions;
-				   }
-				   if( responseCounter === 0 ) {
-					   res.send(tasks);
-				   }
-			   }
-		   }
-		   
-		   for( var i = 0; i < tasks.length; i++ ) {			   
-			   var taskId = tasks[i]._id.toString();			   
-			   
-			   conn.collection('transitions').find({source: taskId}).toArray(function (err, transitions) {		
-				   //console.log(transitions);
-				   addTransition(null, transitions);   
-			   });			   
-		   }		   
-		   
-		       
-	   }
-    });   
-});
-
-
-app.put('/projects/:projectId/states/:id', passport.authenticate('bearer', {session: false}), authorizeUser, function(req, res) {
-	// replace an existing state object
-	
-	//console.log(req.body);
-	//console.log(req.params.id);
-	
-	var task = req.body.task;
-    var title = task.title;
-    var description = task.description;    
-    var responseData = task.response;
-    var methods = task.methods;
-    var transitions = task.transitions;
-    var url = task.url;    
-    var id = req.params.id;
-    
-    var projectId = req.params.projectId;
-    task["project"] = projectId;
-	
-	console.log(task);
-	
-
-	//TODO
-	/*****
-	// make sure that the transitions array is valid
-	conn.collection('transitions').find({source: taskId}).toArray(function (err, storedTransitions) {
-		// convert this array into a hash map
-		var storedTransitionsHash = {};
-		for( var i = 0; i < storedTransitions.length; i++ ) {
-			storedTransitionsHash[storedTransitions[i]._id] = storedTransitions[i];						
-		}
-		
-		for( var i = 0; i < transitions.length; i++ ) {
-			var transition = transitions[i];
-			if( storedTransitionsHash[transition.id] === null ) {
-				// We don't have a record of this transition.  For now we aren't checking the case where a resource is taking over
-				// an existing transition.
-				res.status(400, '{"message" : "Invalid transition id' + transition.id + '"' );
-			}else {
-				// We have this transition, should it be updated?
-				if( )
-			}
-		}
-		
-		
-	}
-	****/
-	conn.collection('tasks').updateById(conn.ObjectID.createFromHexString(id), task, function (err, post) {
-		if( err ) {
-			res.status(500);
-			res.send('{"message" : "Unable to store data in database."}');
-			console.log(err);
-		}else {
-						
-			res.send(200, post);
-		}
-        	        
-    });
-	
-	// Update the mock listeners
-	registerMockListeners();
-	
-});
-
-app.post('/projects/:projectId/states', passport.authenticate('bearer', {session: false}), authorizeUser, function(req, res){
-	
-	// TODO: Make sure this user is authorized to make changes to this project.
-	// TODO: Validate that the project exists, right now it is accepting any string.
-	
-    // Store a newly created task object
-    console.log(req.body);
-    var task = req.body.task;
-    var title = task.title;
-    var description = task.description;
-    var requestData = task.request;
-    var responseData = task.response;
-    var links = task.links;
-    var url = task.url;
-    var id = task.nodeId;
-    var methods = task.methods;
-    
-    task["project"] = req.params.projectId;
-    
-    console.log(id);
-    // Store object in database
-    if( id === undefined) {
-    	console.log(task);
-        conn.collection('tasks').insert(task, function (err, post) {
-        console.log('Error: ' + err);
-        console.log('Post:' + post);
-        
-        // TODO: return the new node id
-        res.send(post);        
-        });    
-    }else {
-    	// TODO: What is this all about?
-        conn.collection('tasks').updateById(conn.ObjectID.createFromHexString(id), task, function (err, post) {
-            console.log('Error: ' + err);
-        console.log('Post:' + post);
-        
-        // TODO: return the updated node id
-        res.send(post);        
-        });    
-    }
-    
-    registerMockListeners();
-    
-       
-});
-
-//TODO: Update this with project and auth support
-app.delete('/projects/:projectId/states/:id', passport.authenticate('bearer', {session: false}), authorizeUser, function(req,res) {
-	var id = req.params.id;
-	var projectId = req.params.projectId;
-	
-	conn.collection('tasks').remove({_id: conn.ObjectID.createFromHexString(req.params.id)}, function (err, task) {
-		conn.collection('transitions').remove({source: id}, function( err, task) {
-			if( err != null ) {
-				console.log('unable to delete transitions');
-				console.log(err);
-			}
-		})
-       res.send('{ "status" : "deleted" }');       
-    });	   	
-})
-
-// TRANSITIONS
-app.post('/projects/:projectId/transitions', passport.authenticate('bearer', {session: false}), authorizeUser, function(req, res){
-//	app.post('/projects/:projectId/transitions', function(req, res){
-	
-    // Store a newly created transition object
-        
-    console.log(req.body);
-    //var transition = req.body.transition;
-    var title = req.body.title;
-    var description = req.body.description;
-    var source = req.body.source;
-    var target = req.body.target;
-    var contentType = req.body.contentType;
-    var url = req.body.url;
-    var method = req.body.method;
-    
-    //TODO: update the methods allowed on the target object    
-    
-    var transition = {
-    		title: title,
-    		description: description,
-    		source: source,
-    		target: target,
-    		contentType: contentType,
-    		url: url,
-    		method: method
-    };
-    
-    transition["project"] = req.params.projectId;
-   
-    	console.log(transition);
-        conn.collection('transitions').insert(transition, function (err, post) {
-        console.log('Error: ' + err);
-        console.log('Post:' + post);
-        console.log(post);
-        
-        // TODO: return the new transition id
-        res.send(post);        
-        });
-        
-        // update the links of the source
-    
-    registerMockListeners();
-    
-       
-});
-
-//TODO: we should be validating against project ID here.
-app.delete('/transitions/:transitionId', passport.authenticate('bearer', {session: false}), authorizeUser, function(req, res){
-	var transitionId = req.params.transitionId;
-	
-		conn.collection('transitions').remove({_id: conn.ObjectID.createFromHexString(transitionId)}, function( err, task) {
-			if( err != null ) {
-				console.log('unable to delete transitions');
-				console.log(err);
-				res.send(500, 'failed.')
-			} else {
-				res.send('{ "status" : "deleted" }');
-			}
-		})                 
-	
-});
 
 // get a list of ALPS profiles
 app.get('/ALPS/profiles', function(req, res) {
@@ -693,18 +426,26 @@ app.post('/projects', passport.authenticate('bearer', {session: false}), functio
 			owner : req.user,
 			created : new Date()
 	}
+
+    project.simpleVocabulary = [];
 			
 	conn.collection('projects').insert(project, function (err, post) {
 		if( err === null ) {
 			//TODO: Check if this is a hypermedia project
 			// If this is a hypermedia project, create the first home resource automatically				
+
+            if( post[0].projectType === 'hypermedia' ) {
 			var home = {
-				title: 'home',
-				description: 'the default starting point',
+				name: 'home',
+				description: 'The default start state.',
+                transitions: [],
+                responses: {primary: ''},
+                uri: '$(home)',
 				project: post[0]._id.toString()
 			}
-			conn.collection('tasks').insert(home, function (err, post) {
+			conn.collection('states').insert(home, function (err, post) {
 			});
+            }
 			res.send(200, post);
 		}else {
 			console.log(err);
@@ -713,6 +454,40 @@ app.post('/projects', passport.authenticate('bearer', {session: false}), functio
 	});
 });
 
+
+app.put('/projects/:projectId', function(req,res) {
+    var projectId = req.params.projectId;
+
+    // Do partial replace by default.
+    // TODO: support other attributes
+    var vocabulary = req.body.simpleVocabulary;
+    var name = req.body.name;
+    var description = req.body.description;
+    var projectType = req.body.projectType;
+    var contentType = req.body.contentType;
+
+    console.log(name);
+    console.log(req.body);
+
+    var updatedContent = {};
+    if( vocabulary ) { updatedContent.simpleVocabulary = vocabulary; }
+    if( name ) { updatedContent.name = name; }
+    if( description ) { updatedContent.description = description; }
+    if( projectType ) { updatedContent.projectType = projectType; }
+    if( contentType ) { updatedContent.contentType = contentType; }
+    console.log(updatedContent);
+
+	conn.collection('projects').update(
+		{_id: mongo.helper.toObjectID(projectId)}, 
+		{'$set': updatedContent },
+		function(err, post) {
+			if( !err ) {
+				res.send(200);
+			} else {
+				res.send(500, err);
+			}
+	});
+});
 
 // Update the simple vocabulary of a project
 app.put('/projects/:projectId/vocabulary', function(req, res) {	
