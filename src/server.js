a4477ba8476383b098022cc01ab3016fec21dca1
Ronnie Mitra <ronnie.mitra@gmail.com>
2017-05-25 15:04:33 +0100

    startup routines were in the wrong order

--- src/server.js
+++ src/server.js
@@ -7,11 +7,10 @@ const config = require('./config.js');
 winston.log('debug', 'loading configuration from rapido.json');
 config.load('../rapido.json');
 
-const serverManager = require('./server-setup.js');
 const dataAccessor = require('../src/db/DataAccessor.js');
-
 winston.log('debug', 'connecting to databse');
 dataAccessor.start(config.database)
 
 winston.log('debug', 'starting server...');
+const serverManager = require('./server-setup.js');
 serverManager.start(config.port);
